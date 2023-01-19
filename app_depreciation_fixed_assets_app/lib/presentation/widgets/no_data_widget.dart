2aeb886e6454b673a788c8e326eb8fc17953da58
Piotr Hariuc <acvatoris@gmail.com>
2023-01-19 12:56:44 +0200

    +

---
+++ app_depreciation_fixed_assets_app/lib/presentation/widgets/no_data_widget.dart
@@ -0,0 +1,26 @@
+import 'package:depreciation_fixed_assets_app/constants/app_constants_index.dart';
+import 'package:flutter/material.dart';
+
+class NoDataWidget extends StatelessWidget {
+  final String noDataTitle;
+
+  const NoDataWidget({Key? key, required this.noDataTitle}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        ImageAssets.noDataIcon(height: AppSize.s50, width: AppSize.s50),
+        const SizedBox(
+          height: AppSize.s8,
+        ),
+        Text(
+          noDataTitle,
+          style: StylesManager.getLightStyle(fontSize: FontSize.s16),
+        )
+      ],
+    );
+  }
+}
